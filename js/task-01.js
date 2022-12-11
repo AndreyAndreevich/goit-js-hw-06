776bf3cde61f16302de6a1b7d0e0b039837f4af5
AndreyAndreevich <andruxa05033@gmail.com>
2022-12-11 23:21:44 +0200

    +

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const categoriesList = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesList.length}`);
+console.log(`Number of categories:${categoriesList.length}`);
 
 categoriesList.forEach((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
